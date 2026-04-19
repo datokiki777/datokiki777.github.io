c0dee64d2e4e6a5617b65c1705935ed7d8d7e63f
Davit <mamulashvili1985@gmail.com>
2026-04-19 19:59:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "client-totals-shell-v5.7";
-const RUNTIME_CACHE = "client-totals-runtime-v5.7";
+const CACHE = "client-totals-shell-v6.0";
+const RUNTIME_CACHE = "client-totals-runtime-v6.0";
 const CDN_CACHE = "client-totals-cdn-v5.7";
 const CDN_ASSETS = [
   "https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js",
